7e2e638b13e8e361366b118f2bef3ec514c50a4c
Tom Pickard <tom@pickard.dev>
2023-03-24 03:59:01 +0000

    I must have done something

--- plugins/user.lua
+++ plugins/user.lua
@@ -1,4 +1,19 @@
 return {
+  {
+    "nguyenvukhang/nvim-toggler",
+    event = "BufRead",
+    config = function()
+      require("nvim-toggler").setup()
+    end,
+  },
+  {
+    "lukas-reineke/virt-column.nvim",
+    event = "BufRead",
+    config = function()
+      require("virt-column").setup()
+    end,
+  },
+
   -- You can also add new plugins here as well:
   -- Add plugins, the lazy syntax
   -- "andweeb/presence.nvim",
