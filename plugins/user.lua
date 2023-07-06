ad0890e67dfa346a92f8af4bbeaed703434f9d60
Tom Pickard <tom@winderwinds.io>
2023-07-05 22:44:27 -0600

    updated colorscheme to rosepine

--- plugins/user.lua
+++ plugins/user.lua
@@ -1,27 +1,31 @@
 return {
-  {
-    "nguyenvukhang/nvim-toggler",
-    event = "BufRead",
-    config = function()
-      require("nvim-toggler").setup()
-    end,
-  },
-  {
-    "lukas-reineke/virt-column.nvim",
-    event = "BufRead",
-    config = function()
-      require("virt-column").setup()
-    end,
-  },
-
-  -- You can also add new plugins here as well:
-  -- Add plugins, the lazy syntax
-  -- "andweeb/presence.nvim",
-  -- {
-  --   "ray-x/lsp_signature.nvim",
-  --   event = "BufRead",
-  --   config = function()
-  --     require("lsp_signature").setup()
-  --   end,
-  -- },
+    {
+        "nguyenvukhang/nvim-toggler",
+        event = "BufRead",
+        config = function() require("nvim-toggler").setup() end,
+    },
+    {
+        "lukas-reineke/virt-column.nvim",
+        event = "BufRead",
+        config = function() require("virt-column").setup() end,
+    },
+    {
+        "catppuccin/nvim",
+        name = "catppuccin",
+        config = function() require("catppuccin").setup {} end,
+    },
+    {
+        "rose-pine/neovim",
+        name = "rose-pine",
+    },
+    -- You can also add new plugins here as well:
+    -- Add plugins, the lazy syntax
+    -- "andweeb/presence.nvim",
+    -- {
+    --   "ray-x/lsp_signature.nvim",
+    --   event = "BufRead",
+    --   config = function()
+    --     require("lsp_signature").setup()
+    --   end,
+    -- },
 }
